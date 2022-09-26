a83b69ddc676b0f1e487e741e6acbc6a052f8803
LimeHawk <kaikelfalcao@gmail.com>
2022-09-26 13:18:58 -0300

    A chechkMate, finally

--- src/UI.java
+++ src/UI.java
@@ -55,9 +55,15 @@ public class UI {
             printCapturedPieces(captured);
             System.out.println();
             System.out.println("Turn : " + chessMatch.getTurn());
-            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-            if(chessMatch.getCheck()){
-                System.out.println("You are in Check (DANGER) !");
+            if(!chessMatch.getCheckMate()){
+                System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+                if(chessMatch.getCheck()){
+                    System.out.println("You are in Check (DANGER) !");
+                }
+            }
+            else{
+                System.out.println("CHECK MATE!");
+                System.out.println("Player " + chessMatch.getCurrentPlayer() + " win the game !");
             }
             
         }
