ecab87ab9740b444de16902ec882a5bf37c59eac
LimeHawk <kaikelfalcao@gmail.com>
2022-09-26 12:00:06 -0300

    List of Captured Pieces update

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -6,6 +6,8 @@ import boardgame.Board;
 import boardgame.BoardException;
 import boardgame.Piece;
 import boardgame.Position;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChessMatch {
@@ -13,6 +15,9 @@ public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+    
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
     
     public ChessMatch(){
         board = new Board(8,8);
@@ -61,6 +66,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+        
+        if(capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
     
@@ -91,6 +101,7 @@ public class ChessMatch {
     
     private void placeNewPiece(char column,int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column,row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
     
     private void initialSetup(){
